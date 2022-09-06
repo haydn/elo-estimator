c588f4a3c0dc6a70f4eeec9c04b76d7af72ae63e
Haydn Ewers <27211+haydn@users.noreply.github.com>
2022-09-06 10:08:52 +1000

    Fix button label

--- components/IssueComparison.tsx
+++ components/IssueComparison.tsx
@@ -61,7 +61,7 @@ const IssueComparison = ({
               loadIssues();
             }}
           >
-            {buttonLabel(issueA)}
+            {buttonLabel(issueB)}
           </button>
         </div>
       </div>
