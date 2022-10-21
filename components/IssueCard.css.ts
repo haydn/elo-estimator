376423c1436086cfcc1f96df60ee2fa199c067b3
Haydn Ewers <27211+haydn@users.noreply.github.com>
2022-10-21 11:18:30 +1100

    Prevent text overflowing on issue cards

--- components/IssueCard.css.ts
+++ components/IssueCard.css.ts
@@ -4,5 +4,6 @@ import theme from "../utils/theme.css";
 export const container = style({
   border: `1px solid ${theme.color.background}`,
   borderRadius: 20,
+  overflow: "hidden",
   padding: 20,
 });
